515f81b8bb28e59f1d958d5e1d0203efa583b695
Jeeva Kandasamy <jkandasa@redhat.com>
2023-01-25 12:16:47 +0530

    copy yaml code to clipboard
    Signed-off-by: Jeeva Kandasamy <jkandasa@redhat.com>

--- ui/src/components/Yaml/index.tsx
+++ ui/src/components/Yaml/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import { ClipboardCopyButton, Split, SplitItem } from '@patternfly/react-core';
+import React, { useState } from 'react';
+
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 interface Props {
@@ -7,10 +9,34 @@ interface Props {
 
 const Yaml: React.FC<Props> = (props: Props) => {
   const { value } = props;
+  const [copied, setCopied] = useState(false);
   return (
-    <SyntaxHighlighter language="yaml" showLineNumbers={true} wrapLines={true}>
-      {value}
-    </SyntaxHighlighter>
+    <>
+      <Split>
+        <SplitItem isFilled />
+        <SplitItem>
+          <ClipboardCopyButton
+            id="basic-copy-button"
+            textId="code-content"
+            aria-label="Copy to clipboard"
+            onClick={() => {
+              navigator.clipboard.writeText(value.toString());
+              setCopied(true);
+            }}
+            exitDelay={copied ? 1500 : 600}
+            maxWidth="220px"
+            variant="plain"
+            onTooltipHidden={() => setCopied(false)}
+            position="auto"
+          >
+            {copied ? 'Successfully copied to clipboard!' : 'Copy to clipboard'}
+          </ClipboardCopyButton>
+        </SplitItem>
+      </Split>
+      <SyntaxHighlighter language="yaml" showLineNumbers={true} wrapLines={true}>
+        {value}
+      </SyntaxHighlighter>
+    </>
   );
 };
 
